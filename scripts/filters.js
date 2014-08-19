f2e09ddb4c5c7c4f04fc6ec59af49abc5376a801
Kevin Swiber <kswiber@gmail.com>
2014-08-19 12:50:24 -0400

    More pruning.

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,31 +1,12 @@
-
-angular.module('zetta').filter('encodeURIComponent', function() {
-    return window.encodeURIComponent;
-  })
-  .filter('prettify', function() {
-    return function(obj) {
-      return JSON.stringify(obj, function(key, val) {
-        return (key === '$$hashKey') ? undefined : val;
-      }, 2);
-    };
-  });
-
-//  Strip "/" and ":" characters from a URL.
-//  TODO: Let this handle the urlEncoded hashes construced by encodeURIComponent
-
-angular.module('zetta').filter('abc123', function(){
-    return function(obj){
-      //this doesn't really make things alphanumeric only, but it'll turn a non-urlencoded url into a valid js ID attribute :)
-      return obj.replace(/\//g, "").replace(/:/g, "");
-    }
-  })
-
-//  Turn common english words into their plural equivalent.
-//  Makes for prettier 1 or many lists, without doing things like "Displaying 1 word(s)".
-
-//  * Smart enough to turn "entry" into "entries"
-
-angular.module('zetta').filter('pluralize', function() {
+angular.module('zetta')
+.filter('prettify', function() {
+  return function(obj) {
+    return JSON.stringify(obj, function(key, val) {
+      return (key === '$$hashKey') ? undefined : val;
+    }, 2);
+  };
+})
+.filter('pluralize', function() {
   return function(ordinal, noun) {
     if (ordinal == 1) {
       return ordinal + ' ' + noun;
@@ -45,86 +26,4 @@ angular.module('zetta').filter('pluralize', function() {
       return ordinal + ' ' + plural;
     }
   };
-})
-.filter('reverse', function() {
-  return function(items) {
-    return items.slice().reverse();
-  };
-})
-.filter('nosub', function() {
-  return function(items) {
-    return items.replace('-subscribe', '');
-  };
-})
-.filter('nodash', function() {
-  return function(input) {
-    return input.replace(/-/g, ' ');
-  }
-})
-.filter('capitalize', function() {
-  return function(input, all) {
-    return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g,
-      function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
-  }
-})
-.filter('mapurl', function() {
-  return function(project) {
-    var tilesUrl = {
-      false: "alanguirand.i6m2j6kf",
-      true: "alanguirand.i04decfa"
-    }
-    var marker = {
-      false: "pin-l+aaa",
-      true: "pin-l-star+f00"
-    }
-
-    var url = "http://api.tiles.mapbox.com/v3/"+tilesUrl[project.online]+"/"+marker[project.online]+"("+project.location.lon+","+project.location.lat+",13)/"+project.location.lon+","+project.location.lat+",13/600x280.png";
-    console.log(url);
-    return url;
-  };
-})
-.filter('largemapurl', function(){
-  return function(project) {
-    var tilesUrl = "alanguirand.i04decfa";
-    var marker = "pin-l+f00";
-    var url =  "http://api.tiles.mapbox.com/v3/"+tilesUrl+"/"+marker+"("+project.location.lon+","+project.location.lat+")/"+project.location.lon+","+project.location.lat+",2/1280x100.png";
-    return url;
-  };
-})
-.filter('deviceurl', function(){
-  return function(device) {
-    var tilesUrl = "alanguirand.i04decfa";
-    var marker = "pin-l+f00";
-    var mapLoc = {
-      lat: 42.331389,
-      lon: -83.045833
-    };
-
-    var deviceMarker = "";
-    if(device.location) {
-      deviceMarker = marker+"("+device.location.lon+","+device.location.lat+")/";
-    }else {
-      deviceMarker = marker+"("+mapLoc.lon+","+mapLoc.lat+")/";
-    }
-    var url =  "http://api.tiles.mapbox.com/v3/"+tilesUrl+"/"+deviceMarker+mapLoc.lon+","+mapLoc.lat+",2/600x160.png";
-    return url;
-  }
-})
-.filter('icon', function() {
-  return function(klass) {
-    //map classes to font awesome icons
-    var def = "fa-cog";
-    var mapping = {
-      arm: "fa-wrench",
-      humidity: "fa-tint",
-      barometer: "fa-flask",
-      temperature: "fa-cloud",
-      huehub: "fa-cog",
-      huebulb: "fa-lightbulb-o",
-      system: "fa-gears",
-      sound: "fa-volume-up",
-      button: "fa-play"
-    }
-    return mapping[klass] || def;
-  }
 });
