6bdbe429ce33d3f8b1d6e509b993737a8b6555bc
Kevin Swiber <kswiber@gmail.com>
2014-08-20 14:38:21 -0400

    Popping stash, looks like the dist script is not cool with it.

--- src/scripts/directives/overview-action.js
+++ src/scripts/directives/overview-action.js
@@ -1,5 +1,18 @@
 angular.module('zetta').directive('zettaOverviewAction', [function() {
   var link = function(scope, element) {
+    if (scope.action.fields) {
+      if (scope.action.fields.length === 1 && scope.action.fields[0].type === 'radio') {
+        scope.action.renderOptionsAsButtons = true;
+      } else {
+        scope.action.renderOptionsAsButtons = false;
+        scope.action.fields.forEach(function(field, i) {
+          if (!field.type) {
+            scope.action.fields[i].type = 'text';
+          }
+        });
+      }
+    }
+
     scope.execute = function() {
       var button = $('.' + scope.action.name + '-button', element);
 
