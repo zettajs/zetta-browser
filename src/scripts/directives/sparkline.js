f572350526c871ca1e9c13e1dd87150c4b149cf3
Alan Languirand <alan@13protons.com>
2014-08-26 15:34:27 -0400

    sparkline labels

--- src/scripts/directives/sparkline.js
+++ src/scripts/directives/sparkline.js
@@ -11,12 +11,31 @@ angular.module('zetta').directive('sparkline', ['$compile', function($compile) {
       x.domain(d3.extent(stream, function(d) {return d.x}));
       y.domain(d3.extent(stream, function(d) {return d.y}));
 
+      
+      
+      
       scope.line = d3.svg.line()
           .x(function(d) {return x(d.x);})
           .y(function(d) {return y(d.y);});
 
       var d = scope.line(stream);
-
+      var mm = {
+        raw: d3.extent(stream.map(function(point){return point.y})),
+        min: null,
+        max: null
+      }
+      
+      stream.forEach(function(point){
+        if(mm.min == null && point.y == mm.raw[0]){
+          mm.min = point;
+        }
+        else if(mm.max == null && point.y == mm.raw[1]){
+           mm.max = point;
+        }
+      });
+      
+      console.log(mm);
+      
       if (d) {
         
        angular.element(element[0].querySelector('.dataline')).attr({"d": d});
