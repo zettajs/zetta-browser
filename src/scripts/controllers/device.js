f985bc3f65eabdacfbdcac6279b6438c576566d9
Kevin Swiber <kswiber@gmail.com>
2014-08-26 15:27:37 -0400

    Cleaning up device controller.

--- src/scripts/controllers/device.js
+++ src/scripts/controllers/device.js
@@ -1,9 +1,7 @@
 angular.module('zetta').controller('DeviceCtrl', [
-  '$scope', '$state', '$http', 'navigator', 'zettaShared', function($scope, $state, $http, navigator, zettaShared) {
+  '$scope', '$state', 'zettaShared', function($scope, $state, zettaShared) {
   $scope.device = null;
   $scope.request = null;
-  $scope.response = null;
-  $scope.servers = zettaShared.state.servers;
 
   $scope.init = function() {
     var rootUrl = zettaShared.state.root;
@@ -19,6 +17,13 @@ angular.module('zetta').controller('DeviceCtrl', [
       zettaShared.state.root = rootUrl;
     }
 
+    var anchor = document.createElement('a');
+    anchor.href = $state.params.url;
+
+    $scope.request = ['GET ' + anchor.pathname,
+      'Host: ' + anchor.hostname,
+      'Accept: application/vnd.siren+json'].join('\r\n');
+
     if (!zettaShared.state.servers.length) {
       zettaShared.state.loadServers(rootUrl, zettaShared.state.execute, function() {
         findDevice();
@@ -27,27 +32,21 @@ angular.module('zetta').controller('DeviceCtrl', [
       findDevice();
     };
 
-
     $scope.stateLogs = [];
   };
 
   var findDevice = function() {
-    var anchor = document.createElement('a');
-    anchor.href = $state.params.url;
-
-    $scope.request = ['GET ' + anchor.pathname,
-      'Host: ' + anchor.hostname,
-      'Accept: application/vnd.siren+json'].join('\r\n');
-
     zettaShared.state.servers.forEach(function(server) {
       server.devices.forEach(function(device) {
         if (device.href === $state.params.url) {
           $scope.device = device;
           $scope.logger(device.monitorHref);
 
-          zettaShared.state.breadcrumbs = [ { title: 'root', href: zettaShared.state.root }, { title: server.name, href: server.href },
-            { title: device.properties.name || device.properties.type , href: $state.params.url }];
-
+          zettaShared.state.breadcrumbs = [
+            { title: 'root', href: zettaShared.state.root },
+            { title: server.name, href: server.href },
+            { title: device.properties.name || device.properties.type , href: $state.params.url }
+          ];
         }
       });
     });
@@ -89,47 +88,10 @@ angular.module('zetta').controller('DeviceCtrl', [
           }
         });
 
-        if($scope.stateLogs.length > 10){ $scope.stateLogs.pop() } //keep things civil
+        if($scope.stateLogs.length > 10) {
+          $scope.stateLogs.pop()
+        }
       });
     }
   };
-  
-  var follow = function(rootUrl) {
-    var url = rootUrl;
-
-    $scope.url = url;
-
-    $state.params.url = url;
-
-    var anchor = document.createElement('a');
-    anchor.href = url;
-
-    $scope.request = ['GET ' + anchor.pathname,
-      'Host: ' + anchor.hostname,
-      'Accept: application/vnd.siren+json'].join('\r\n');
-
-    navigator.redirectOrFetch(url, $state.params).then(function(data) {
-      $scope.response = {};
-
-      ['class', 'properties', 'entities', 'actions', 'links'].forEach(function(prop) {
-        if (data.hasOwnProperty(prop)) {
-          $scope.response[prop] = data[prop];
-        }
-      });
-
-      if ($scope.response.actions) {
-        $scope.response.actions = $scope.response.actions.map(function(action) {
-          var a = {};
-          ['class', 'name', 'method', 'href', 'fields'].forEach(function(prop) {
-            if (action.hasOwnProperty(prop)) {
-              a[prop] = action[prop];
-            }
-          });
-
-          return a;
-        });
-      }
-    });
-  };
-
 }]);
