ce7765ca0d61a9461e8c03c11add753406055f14
Alan Languirand <=>
2014-09-02 17:22:26 -0400

    Prism Works

--- src/scripts/directives/prism.js
+++ src/scripts/directives/prism.js
@@ -1,11 +1,16 @@
 angular.module('zetta').directive('prism', [function() {
-        return {
-            restrict: 'A',
-            link: function ($scope, element, attrs) {
-                element.ready(function() {
-                    Prism.highlightElement(element[0]);
-                });
-            }
-        } 
-    }]
-);
+    return {
+        restrict: 'A',
+        scope: {
+            source: '@'
+        },
+        link: function(scope, element, attrs) {
+            scope.$watch('source', function(v) {
+                if(v) {
+                    Prism.highlightElement(element.find("code")[0]);
+                }
+            });
+        },
+        template: "<code ng-bind='source'></code>"
+    };
+}]);
