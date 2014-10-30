a1d56cf9789f09269e27eb5343dcd1a52f7c56ff
Alan Languirand <alan@13protons.com>
2014-10-30 17:23:27 -0400

    pass-through field type for most html fields for templating

--- src/scripts/directives/action-field.js
+++ src/scripts/directives/action-field.js
@@ -2,12 +2,14 @@ angular.module('zetta').directive('actionField', [function() {
 
     function link(scope, element, attrs) {
       var template = scope.field.type;
+      var passThrough = ['hidden', 'text', 'search', 'tel', 'url', 'email', 'password', 'datetime', 'date', 'month', 'week', 'time', 'datetime-local', 'number', 'color', 'checkbox', 'file', 'image', 'button']
+      
       var valid = ['hidden', 'text', 'search', 'tel', 'url', 'email', 'password', 'datetime', 'date', 'month', 'week', 'time', 'datetime-local', 'number', 'range', 'color', 'checkbox', 'radio', 'file', 'image', 'button'];
       
       if(valid.indexOf(scope.field.type) == -1){template = 'default'} //attempt to fail fast and gracefully
-      else if(scope.field.type == 'text'){ template = 'default' }
-      else if(['number', 'range'].indexOf(scope.field.type) > -1){ template = 'number'; } //normalize
-      else if(['select', 'radio'].indexOf(scope.field.type) > -1){ template = 'dropdown'; } //normalize
+      else if(passThrough.indexOf(scope.field.type) > -1){ template = 'default' }
+      else if(['range'].indexOf(scope.field.type) > -1){ template = 'number'; } 
+      else if(['select', 'radio'].indexOf(scope.field.type) > -1){ template = 'dropdown'; }  //normalize
 
       scope.contentUrl = 'partials/fields/'+template+'.html'
       //scope.contentUrl = 'partials/fields/default.html'
